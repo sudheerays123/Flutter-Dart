617a23d453e9776d998aaa1e76f6981f011eb716
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2020-09-30 12:51:28 +0530

    Update main.dart

--- Meals App/main.dart
+++ Meals App/main.dart
@@ -1,1 +1,74 @@
+import 'package:MealsAPP/models/meal.dart';
+import 'package:MealsAPP/screens/categories_meals_screen.dart';
+import 'package:MealsAPP/screens/filters_screen.dart';
+import 'package:flutter/material.dart';
+import 'screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/filters_screen.dart';
+import 'package:MealsAPP/dummy_data.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Meal> favouriteMeals = [];
+
+  void ToggleFavourite(String mealId) {
+    final existingIndex =
+        favouriteMeals.indexWhere((meal) => meal.id == mealId);
+    if (existingIndex >= 0) {
+      setState(() {
+        favouriteMeals.removeAt(existingIndex);
+      });
+    } else {
+      setState(() {
+        favouriteMeals.add(
+          DUMMY_MEALS.firstWhere((meal) => meal.id == mealId),
+        );
+      });
+    }
+  }
+
+  bool isMealFavourite(String id) {
+    return favouriteMeals.any((meal) => meal.id == id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'RobotoCondensed'),
+            ),
+      ),
+      routes: {
+        '/': (ctx) => TabsScreen(favouriteMeals),
+        '/each-topic': (ctx) => CategoryMealsScreen(),
+        MealDetail.routeName: (ctx) => MealDetail(
+              ToggleFavourite,
+              isMealFavourite,
+            ),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
+      },
+    );
+  }
+}
