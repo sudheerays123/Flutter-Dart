8c092a3c0ccb5d4466392599a54d44b4b1763758
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2020-10-24 13:17:57 +0530

    Update main.dart

--- shop app/main.dart
+++ shop app/main.dart
@@ -1,20 +1,29 @@
 import 'package:ShopAPP/screens/products_overview_screen.dart';
 import 'package:flutter/material.dart';
+import './screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import './providers/products_provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen(),
     );
   }
 }
