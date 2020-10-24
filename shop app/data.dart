86274fbf8b4a896329f7f4a24431370a11d94b9b
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2020-10-24 13:17:40 +0530

    Update data.dart

--- shop app/data.dart
+++ shop app/data.dart
@@ -26,10 +26,88 @@ final List<Product> allProducts = [
   ),
   Product(
     id: 'p4',
-    title: 'A Pan',
+    title: 'Pan',
     description: 'Prepare any meal you want.',
     price: 49.99,
     imageURL:
         'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
+  ),
+  Product(
+      id: 'p5',
+      title: 'Book',
+      description: 'Study anything',
+      price: 10.99,
+      imageURL:
+          'https://assets.entrepreneur.com/content/3x2/2000/20191219170611-GettyImages-1152794789.jpeg?width=700&crop=2:1'),
+  Product(
+      id: 'p6',
+      title: 'Laptop',
+      description: 'Code',
+      price: 500.89,
+      imageURL:
+          'https://images-na.ssl-images-amazon.com/images/I/71BFWj3oHoL._SL1500_.jpg'),
+  Product(
+    id: 'p7',
+    title: 'Pen',
+    description: 'Write ANYTHING',
+    price: 5.71,
+    imageURL:
+        'https://images-na.ssl-images-amazon.com/images/I/41ZS0PjBW6L._AC_.jpg',
+  ),
+  Product(
+    id: 'p8',
+    title: 'Whiteboard',
+    description: 'Draw',
+    price: 600.26,
+    imageURL:
+        'https://images-na.ssl-images-amazon.com/images/I/71tTK5smdKL._AC_SL1500_.jpg',
+  ),
+  Product(
+    id: 'p9',
+    title: 'Headphone',
+    description: 'Hear',
+    price: 999,
+    imageURL:
+        'https://www.bhphotovideo.com/images/images1000x1000/beats_by_dr_dre_mq562ll_a_studio3_wireless_bluetooth_headphones_1360328.jpg',
+  ),
+  Product(
+    id: 'p10',
+    title: 'Speakers',
+    description: 'Louder',
+    price: 100.34,
+    imageURL:
+        'https://images-na.ssl-images-amazon.com/images/I/512syhuJD7L._SL1000_.jpg',
+  ),
+  Product(
+    id: 'p11',
+    title: 'Bag',
+    description: 'Store',
+    price: 78.35,
+    imageURL:
+        'https://images-na.ssl-images-amazon.com/images/I/815O9y5VD%2BL._SL1500_.jpg',
+  ),
+  Product(
+    id: 'p12',
+    title: 'Pillow',
+    description: 'Sleep',
+    price: 64.35,
+    imageURL:
+        'https://ii1.pepperfry.com/media/catalog/product/w/h/568x625/white-polyester-16-x-24-inch-pillow-by-gilson-white-polyester-16-x-24-inch-pillow-by-gilson-d6sydf.jpg',
+  ),
+  Product(
+    id: 'p13',
+    title: 'Table',
+    description: 'Study',
+    price: 72.90,
+    imageURL:
+        'https://www.ikea.com/in/en/images/products/lisabo-table-black__0737106_PE740884_S5.JPG?f=g',
+  ),
+  Product(
+    id: 'p14',
+    title: 'Box',
+    description: 'Keep',
+    price: 2.90,
+    imageURL:
+        'https://customboxbuilder.com/images/home/Box_Selection_-_RSC_Closed_(Initial_State).png',
   ),
 ];
