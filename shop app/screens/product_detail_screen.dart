e2b745afe2d04e7b0dc8218e7b579061bfa482b4
Sudheera Y S <59356331+sudheerays123@users.noreply.github.com>
2020-12-01 10:19:39 +0530

    Update product_detail_screen.dart

--- shop app/screens/product_detail_screen.dart
+++ shop app/screens/product_detail_screen.dart
@@ -1,16 +1,23 @@
+import 'package:ShopAPP/providers/products_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
   static const routeName = '/product-detail';
 
   @override
   Widget build(BuildContext context) {
-
     final productId = ModalRoute.of(context).settings.arguments as String;
 
+    final loadedpProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).items.firstWhere((prod) => prod.id == productId);
+
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
+      appBar: AppBar(
+        title: Text(loadedpProduct.title),
+      ),
     );
   }
 }
